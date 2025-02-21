4bfb6a81a3dd1ee7c2aefa2eac97fcb3278bc22a
Fredrikvk2002 <fviksekarlse.ieu2023@student.ie.edu>
2025-02-21 11:25:12 +0100

    Question 4

---
+++ Question 4.py
@@ -0,0 +1,8 @@
+my_list = ["a", "b", "c"]
+print("Original list:", my_list)
+
+my_list[1] = "x"  # Changing an element
+print("Modified list:", my_list)
+
+my_list.append("d")  # Adding an element
+print("List after append:", my_list)
